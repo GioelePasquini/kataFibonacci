91034d93b571c846201fd60be62412c86a5a2e95
Gioele <gioele.pasquini101@asu.ulpgc.es>
2022-01-21 10:20:41 +0000

    green: testing_fibonacci_of_8

--- src/main/java/FibonacciAnalyzer.java
+++ src/main/java/FibonacciAnalyzer.java
@@ -1,5 +1,16 @@
 public class FibonacciAnalyzer {
-    public int searchFibonacciNumber(int i) {
-        return 0;
+    public int searchFibonacciNumber(int number) {
+        if(number <= 1) {
+            return number;
+        }
+        int fib = 1;
+        int prevFib = 1;
+
+        for(int i=2; i<number; i++) {
+            int temp = fib;
+            fib+= prevFib;
+            prevFib = temp;
+        }
+        return fib;
     }
 }
